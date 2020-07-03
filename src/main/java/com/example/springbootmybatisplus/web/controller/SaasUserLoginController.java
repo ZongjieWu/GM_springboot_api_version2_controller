50b0c500bcd570270276e2ee8d8bb9f4be1ca52d
ZongjiesWu <2012293155@qq.com>
2020-07-03 19:30:26 +0800

    提交示例代码

---
+++ src/main/java/com/example/springbootmybatisplus/web/controller/SaasUserLoginController.java
@@ -0,0 +1,37 @@
+package com.example.springbootmybatisplus.web.controller;
+
+import com.example.springbootmybatisplus.config.apiversionconfig.ApiVersion;
+import com.example.springbootmybatisplus.enums.Result;
+import com.example.springbootmybatisplus.model.vo.request.saasuser.user.SaasUserLoginRequestVo;
+import com.example.springbootmybatisplus.model.vo.response.SaasUserBaseInfoResponseVo;
+import com.example.springbootmybatisplus.service.ISaasUserLoginService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "1.平台管理员",description = "登入等接口")
+@RestController
+@ApiVersion(2)
+@RequestMapping("{version}/admin")
+public class SaasUserLoginController {
+    @Autowired
+    private ISaasUserLoginService saasUserLoginService;
+
+    @ApiOperation(value = "登入",notes = "登入")
+    @PostMapping("/login")
+    @ApiVersion(6)
+    public Result<SaasUserBaseInfoResponseVo> add(@Validated SaasUserLoginRequestVo loginRequestVo) {
+        return saasUserLoginService.login(loginRequestVo);
+    }
+
+    @ApiOperation(value = "登入2",notes = "登入2")
+    @PostMapping("/login")
+    public Result<SaasUserBaseInfoResponseVo> add2(@Validated SaasUserLoginRequestVo loginRequestVo) {
+        return saasUserLoginService.login(loginRequestVo);
+    }
+
+}
---
+++ src/test/java/com/example/version2controller/Version2controllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.version2controller;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Version2controllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
